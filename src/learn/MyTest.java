855dc1b3d645d893dfdf2567525a6f72443f56d8
dela <jiangmingxia@gmail.com>
2016-09-22 17:05:09 -0700

    quick

--- src/learn/MyTest.java
+++ src/learn/MyTest.java
@@ -1,12 +1,20 @@
 package learn;
 
 import org.testng.annotations.Test;
+
+import solutions.*;
+
 import org.testng.annotations.BeforeClass;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
 public class MyTest {
   @Test
   public void f() {
+	  int[] input = new int[]{1,3,4,58,9,11};	  
+	  boolean res = false;
+	  Solution255 s = new Solution255();
+	  Assert.assertEquals(s.verifyPreorder(input), res);
   }
   @BeforeClass
   public void beforeClass() {
--- src/solutions/Solution255.java
+++ src/solutions/Solution255.java
@@ -1,5 +1,9 @@
 package solutions;
 
 public class Solution255 {
+	public boolean verifyPreorder(int[] preorder) {
+        if (preorder.length == 0) return true;
+        return false;
+    }
 
 }
